d532f928b0673f193a55a823656e646a0f48cb45
asknishant <asknishant.39@gmail.com>
2022-06-19 16:36:25 +0530

    Giving some final touch

--- src/Header/DropDown.js
+++ src/Header/DropDown.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import './DropDown.css'
+import { OpenInBrowserOutlined } from '@mui/icons-material'
+
+export const DropdownRow = ({ idx, text}) => {
+    const icons = [<OpenInBrowserOutlined />,null, null, null]
+  return (
+      <div style={{display: 'flex', margin: 10, justifyContent: 'space-between', fontWeight: '20', fontSize: 14}}>
+        {text} {<OpenInBrowserOutlined />} 
+      </div>
+    
+  )
+}
 
 
 export const DropdownItem = () => {
+    const items = ['Account', 'Profile', 'Upgrade to premium', 'Log out']
+    
   return (
-    <div>Item</div>
+    <div>
+        {items.map((text, idx) => (
+             <DropdownRow icon={idx} text={text}/>
+        ))}
+       
+    </div>
   )
 }
 
